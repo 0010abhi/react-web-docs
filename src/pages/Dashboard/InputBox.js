4d709c3768f4689efc97dc0a4033922403323042
Abhishek Sachdeva <0010abhi@gmail.com>
2021-07-22 13:33:10 +0530

    code review changes done

--- src/pages/Dashboard/InputBox.js
+++ src/pages/Dashboard/InputBox.js
@@ -1,65 +1,48 @@
-import React from 'react';
-import TextField from '@material-ui/core/TextField';
-import { Button } from '@material-ui/core';
-import { useState } from 'react';
-import { database } from '../Firebase';
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import { Button } from "@material-ui/core";
+import { useState } from "react";
+import { database } from "../Firebase";
 
-// import { useRef } from 'react';
+export default function InputBox() {
+  const [text, setText] = useState("");
 
-export default function InputBox(){
+  function handleText(value) {
+    setText(value);
+  }
 
-    const[text, setText]=useState('');
+  //connection with realtime database after click on submit button
+  function handleSubmit() {
+    console.log(text);
+    console.log("hey", database);
+    database
+      .ref("users")
+      .set({
+        text: text,
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  }
 
-    // const valueRef1 = useRef('')
-    // const valueRef2 = useRef('') 
-    // function handleSubmit(){
-    //     console.log(valueRef1.current.value , valueRef2.current.value) 
-    // }
-     //console.log(event.target.value)
-        // setText(event.target.value);
-    // inputRef={valueRef1}
-
-    function handleText(value){
-        setText(value);
-       //console.log(value);   
-    }
-
-    //connection with realtime database after click on submit button 
-    function handleSubmit(){
-        console.log(text)
-        console.log('hey',database)
-        database.ref('users').set({
-            text : text
-        }).catch((err) => {
-            console.log('err', err)
-          });
-    }
-
-    // function getData(){
-    //     console.log(text)
-    // }
-   
-    return(
-        <div>
-            <div style={{marginTop:'25px'}}>
-                <TextField label="First Name" variant="outlined" value={text} onChange={(event) => {
-                    handleText(event.target.value); 
-                    // console.log(event.target.value)
-                }} /> 
-            </div>
-            {/* <div style={{marginTop:'25px'}}>
-                <TextField label="Last Name" variant="outlined" onChange={() => {
-                    handleText()
-                }} />
-            </div> */}
-            <div style={{marginTop:'15px'}}>
-                <Button variant="contained" color="primary" onClick={handleSubmit}>
-                    Submit</Button>
-            </div>
-            {/* <div style={{marginTop:'15px'}}>
-                <Button variant="contained" color="primary" onClick={getData}>
-                    Check</Button>
-            </div> */}
-        </div>
-    )
+  return (
+    <div>
+      <div style={{ marginTop: "25px" }}>
+        <TextField
+          label="First Name"
+          variant="outlined"
+          value={text}
+          onChange={(event) => {
+            handleText(event.target.value);
+            // console.log(event.target.value)
+          }}
+        />
+      </div>
+      <div style={{ marginTop: "15px" }}>
+        <Button variant="contained" color="primary" onClick={handleSubmit}>
+          Submit
+        </Button>
+      </div>
+    </div>
+  );
 }
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,20 +1,20 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-export default function LogIn(props){
-  const {pageTitle} = props;
-  const x = 'hello gello';
-  const [email, setEmail] = useState('')
+export default function LogIn(props) {
+  const { pageTitle } = props;
+  const x = "hello gello";
+  const [email, setEmail] = useState("");
 
   function handleEmailChange(e) {
     console.log(e);
-    setEmail(e.target.value)
+    setEmail(e.target.value);
   }
 
-  return(
+  return (
     <div>
       log in {x}
-{pageTitle} {email}
-      <input type='text' value={email} onChange={handleEmailChange} />
+      {pageTitle} {email}
+      <input type="text" value={email} onChange={handleEmailChange} />
     </div>
-  )
+  );
 }
